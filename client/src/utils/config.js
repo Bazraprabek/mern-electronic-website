219eeb10885d9682e15dcd7de13ea1c9ec8465f3
bazraprbek <bazraprbek@gmail.com>
2024-07-18 16:39:15 +0545

    minor change

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,11 +1,14 @@
-// config.js
-const isDevelopment = import.meta.env.MODE === "development";
+const {
+  VITE_MODE,
+  VITE_LOCAL_URL,
+  VITE_PROD_URL,
+  VITE_LOCAL_IMG_PATH,
+  VITE_PROD_IMG_PATH,
+} = import.meta.env;
 
-const baseURL = isDevelopment
-  ? import.meta.env.VITE_LOCAL_URL
-  : import.meta.env.VITE_PROD_URL;
-const imgPath = isDevelopment
-  ? import.meta.env.VITE_LOCAL_IMG_PATH
-  : import.meta.env.VITE_PROD_IMG_PATH;
+const isDevelopment = VITE_MODE === "development";
+
+const baseURL = isDevelopment ? VITE_LOCAL_URL : VITE_PROD_URL;
+const imgPath = isDevelopment ? VITE_LOCAL_IMG_PATH : VITE_PROD_IMG_PATH;
 
 export { isDevelopment, baseURL, imgPath };
